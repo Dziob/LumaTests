0f368b3a1d7c1e46906ab80091648f2cfd2a95b8
mtoma <mtomaszewska2@gmail.com>
2023-05-29 15:05:52 +0200

    POM init

--- LumaTests/Pages/HomePage.cs
+++ LumaTests/Pages/HomePage.cs
@@ -1,11 +1,24 @@
 ﻿using OpenQA.Selenium;
 using SeleniumExtras.PageObjects;
+using System.Runtime.CompilerServices;
+
 
 namespace LumaTests.Pages
 {
 
     class HomePage
     {
+        public IWebDriver driver;
+
+        public HomePage(IWebDriver driver)
+
+        {
+            this.driver = driver;
+            PageFactory.InitElements(driver, this);
+        }
+        
+        IWebElement menuWoman;
+
         
     }
 }
